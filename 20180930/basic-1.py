a42f54ad4a4d1e2bd05a7833c7ecf66b1d69e2dd
paramita <cpeng0907@163.com>
2018-09-30 18:18:35 +0800

    basic info
    basic info

---
+++ 20180930/basic-1.py
@@ -0,0 +1,17 @@
+# 数组
+classmates = ['','Bob','Tracy',1,['asp','jsp'],True]
+classmates.append('aaaa')
+classmates.insert(1,'jack')
+print(classmates)
+print(len(classmates))
+print(classmates[-1])
+print(classmates.pop())
+print(classmates)
+
+# Tuple
+classmates1=('Michael','Bob','Tracy')
+print(classmates1)
+# classmates1[0]=1;
+print (classmates1[1])
+print((1))
+print((1,))
