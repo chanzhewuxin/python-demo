b859be2538a4204670307dd6ba30b8879288380f
daniel chen <cpeng0907@gmail.com>
2018-10-23 01:24:48 +0800

    20181023

---
+++ 20181022/GUIDemo.py
@@ -0,0 +1,25 @@
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        # self.helloLabel = Label(self, text='Hello, world!')
+        # self.helloLabel.pack()
+        self.nameInput=Entry(self)
+        self.nameInput.pack()
+        self.quitButton = Button(self, text='Hello', command=self.hello)
+        self.quitButton.pack()
+
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        messagebox.showinfo('Message', 'Hello,%s' % name)
+
+
+app = Application()
+app.master.title('Hello world')
+app.mainloop()
---
+++ 20181022/SQLliteDemo.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+# conn = sqlite3.connect('test.db')
+# cursor = conn.cursor()
+# cursor.execute('create table user (id varchar(20) primary key,name varchar(20))')
+# cursor.execute('insert into user (id, name) values (\'1\', \'Michael\')')
+# print(cursor.rowcount)
+# cursor.close()
+# conn.commit()
+# conn.close()
+
+conn = sqlite3.connect('test.db')
+cursor=conn.cursor()
+cursor.execute('select * from user where id=?',('1',))
+values=cursor.fetchall()
+print(values)
+cursor.close()
+conn.close()
